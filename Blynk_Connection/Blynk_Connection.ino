97f0a9b243d92f39b201458ffbc59ded671fc400
codewithindunil <codewithindunil@gmail.com>
2019-12-08 11:23:03 +0530

    Connection code

--- Blynk_Connection/Blynk_Connection.ino
+++ Blynk_Connection/Blynk_Connection.ino
@@ -2,17 +2,18 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Hv2X9PaMzbUK1hmhLm5vLvkjsX4Zehrj";
+char auth[] = "YourAuthToken";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "Dialog 4g";
+char ssid[] = "YourNetworkName";
 char pass[] = "YourPassword";
 
 void setup()
@@ -21,6 +22,9 @@ void setup()
   Serial.begin(9600);
 
   Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
 void loop()
